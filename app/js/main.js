fc291744e3b9c6f395f5dac5223a095fadd1f8f6
SergeiKudlai <s.kudlai@mail.ru>
2021-11-22 13:06:42 +0300

    part 13

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,14 @@
 $(function () {
 
-  $('.slider-box').slick({
+  $('.slider__box').slick({
 
-    prevArrow: '<svg class="slick-left" width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M21.0382 31.0149L37.0515 45.6079C37.577 46.1307 38.4292 46.1307 38.9546 45.6079C39.4801 45.0851 39.4801 44.2369 38.9546 43.7141L23.9069 30L38.9533 16.2859C39.4788 15.7631 39.4788 14.9148 38.9533 14.3921C38.4278 13.8693 37.5756 13.8693 37.0502 14.3921L21.0369 28.9851C20.7568 29.2637 20.6367 29.6332 20.6554 29.9986C20.638 30.3654 20.7581 30.7348 21.0382 31.0149Z" fill="#505050"/></svg>',
+    prevArrow: $('.arrow__button--left'), 
 
-    nextArrow: '<svg class="slick-right" width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="60" height="60" rx="6" fill="white" fill-opacity="0.8"/><path d="M38.9641 28.9851L22.9508 14.3921C22.4254 13.8693 21.5731 13.8693 21.0477 14.3921C20.5222 14.9149 20.5222 15.7631 21.0477 16.2859L36.0954 30L21.049 43.7141C20.5235 44.2369 20.5235 45.0852 21.049 45.6079C21.5745 46.1307 22.4267 46.1307 22.9521 45.6079L38.9654 31.0149C39.2455 30.7362 39.3656 30.3668 39.3469 30.0014C39.3643 29.6346 39.2443 29.2652 38.9641 28.9851Z" fill="#505050"/></svg>'
-
+    nextArrow: $('.arrow__button--right'), 
+     
   });
 
-
-  var Mixer = mixitup('.product-items', {
+  var Mixer = mixitup('.goods__inner', {
     load: {
       filter: 'all'
     },
@@ -18,11 +17,31 @@ $(function () {
     }
   });
 
-  $('.button-box__btn').on('click', function(){
-      $('.button-menu__drop').toggleClass('button-menu__drop--active');
-      $('.button-box__btn').toggleClass('button-box__btn--active')
-      $('.button-menu').toggleClass('button-menu--active')
+
+  var Mixerr = mixitup('.stock__inner', {
+    load: {
+      filter: 'all'
+    },
+    animation: {
+      animateResizeTargets: true
+    }
   });
+
+
+
+  $('.catalog__button').on('click', function () {
+    $('.catalog__menu').fadeToggle();
+    $('.catalog__button').toggleClass('catalog__button--active');
+  });
+
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest(".catalog__button").length) {
+      $('.catalog__menu').hide();
+      $('.catalog__button').removeClass('catalog__button--active');
+    }
+    e.stopPropagation();
+  });
+
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,30 @@
-//gulpfile для ввода команд в gulp
-//создаём переменные которым передаём возможности gulp
 const { src, dest, watch, parallel, series } = require('gulp');
-/*создаём переменную scss передаём с помощью команды require все возможности gulp-sass а также sass который конвертирует css в scss*/
 const scss = require('gulp-sass')(require('sass'));
-//постоянная concat которой передаём возможности gulp-concat который объеденяет все файлы в один
 const concat = require('gulp-concat');
-//постоянная для отображения данных в браузере
 const browserSync = require('browser-sync').create();
-//постоанная для работы js default такие требования для данного gulpa
 const uglify = require('gulp-uglify-es').default;
-//поятоснная для лучшей работы с более позднеми браузерами кросбраузерность
 const autoprefixer = require('gulp-autoprefixer');
-//постоянна для удаления контента ,объектов
 const del = require('del');
 const sprite = require('gulp-svg-sprite');
 
 
 
-//создаём функцию задаём имя browsersync для создания сервера
 function browsersync() {
 	browserSync.init({
 		server: {
-			//папка для отслеживание сервера 
 			baseDir: "app/"
 		},
-		//в углу не появляется строчка обновления
 		notify: false
 	})
 }
 
 
-//удаляет папку дист
 function cleanDist() {
 	return del('dist')
 }
 
 function svgSprite() {
-	return src('app/images/svg/styles__svg/**/*.svg')
+	return src('app/images/svg/**/*.svg')
 		.pipe(sprite({
 			mode: {
 				stack: {
@@ -64,55 +52,38 @@ function svgSprite() {
 }
 
 
-//создаём функцию scripts для работы с js
 function scripts() {
-	//указывается путь в main.js но в основном еще используются бибилиотеки js
-	//[]означает что будет несколько файлов с расширением js, и последний будет main.js
 	return src([
-		//подключаем файлы js
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/slick-carousel/slick/slick.js',
 		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 		'node_modules/mixitup/dist/mixitup.js',
 		'app/js/main.js'
 	])
-		//Объеденяем в единный файл и переименовываем
 		.pipe(concat('main.min.js'))
-		//данный плагин отвечает за минифицирование
 		.pipe(uglify())
-		//после переименования и минифицирования ложим файл в папку js
 		.pipe(dest('app/js'))
-		//когда меняеться информаци в js страница(сервер) обновляется
 		.pipe(browserSync.stream())
 }
 
 
-//создаём функцию  и задаём имя styles данная функция конвертирует из scss в css 
+
 function styles() {
-	//src работает благодаря тому , что мы передали возможности gulp подключаем и находим файл style.scss
 	return src([
 		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
 		'app/scss/style.scss'
 	])
-		//передаём возможности конвертации благодаря постоянной scss
-		//compressed,expanded добовляет минифицированость в файле css все пишеться в одну строку
 		.pipe(scss({ outputStyle: 'compressed' }))
-		//постоянная concat может переиминовывать файлы а также обхеденять js scss html 
 		.pipe(concat('style.min.css'))
-		//пред тем как внести в папку подключаем автопрефиксер отслеживает на 10 последних версиях браузера,работает с гридами
 		.pipe(autoprefixer({
-			//использовать для последних 10 версий браузеров
 			overrideBrowserslist: ['last 10 version'],
 			grid: true
-		}))
-		//постоянной (dest= присвоили возможности gulp) мы указываем путь куда файл scss будет переносить сконвертированные данные 
+		})) 
 		.pipe(dest('app/css'))
-		//когда меняеться информаци в scss страница(сервер) stream добовляет стили без перегагрузки страницы
 		.pipe(browserSync.stream())
 }
 
 
-//данная функция собарает все файлы и выкидывает в чистовую папку
 function build() {
 	return src([
 		'app/css/style.min.css',
@@ -120,43 +91,30 @@ function build() {
 		'app/js/main.min.js',
 		'app/*.html',
 		'app/images/**/*.*'
-		//данный код позволяет выкидываь по папкам как в app а не файлами по отдельности 
 	], { base: 'app' })
 		.pipe(dest('dist'))
 }
 
 
-//автомотическое слежение за проектом за их изменениями
-//создаём функцию под именем watching слежение
+
 function watching() {
-	//обращаемся к переменной и указываем путь к файлу за которым надо следить
-	//**за всеми внутринними файлами и папками *за всеми файлами с расширениями .scss
-	//при появлении изменений в папке scss функция watching запускает функцию styles
 	watch(['app/scss/**/*.scss'], styles);
-	watch(['app/images/svg/styles__svg/**/*.svg'],svgSprite);
-	//при появлении изменений в папке js функция запускает функцию scripts
-	//следит за всеми файлами js кроме > ! перестаёт следить за файлом main.min.js        
+	watch(['app/images/svg/**/*.svg'],svgSprite);    
 	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-	//слежение за файлами html и обновление страницы
 	watch(['app/*.html']).on('change', browserSync.reload);
 }
 
-//запуск функции styles
+
 exports.styles = styles;
-//запуск функцию слежения watching
 exports.watching = watching;
-//запуск функции сервера browsersync
 exports.browsersync = browsersync;
-//запуск функции java scripts
 exports.scripts = scripts;
-//запуск функции удаления папки дист
 exports.cleanDist = cleanDist;
 exports.svgSprite = svgSprite;
 
-//series свойства gulp выполнять по очереди как заданно
+
 exports.build = series(cleanDist, build);
 
 
-//таск по дефолту(defult) прописывая в терминале gulp запускаеться заданое значение 
-//свойства parallel разрешает запускать одновременно несколько задач в терминале
+
 exports.default = parallel(styles,scripts, browsersync,svgSprite,watching);
